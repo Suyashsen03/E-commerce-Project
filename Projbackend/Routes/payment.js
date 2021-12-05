dbc0dbb5125d6eaad28e8ec9b97bc4ca989a263f
Suyashsen03 <91673270+Suyashsen03@users.noreply.github.com>
2021-12-05 19:54:47 +0530

    Update payment.js

--- Projbackend/Routes/payment.js
+++ Projbackend/Routes/payment.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const sign = require("../Controllers/sign");
 
 const stripe = require("stripe")(
-  "sk_test_51JxcamSCacZxdBMWsfPfvNHbbj5PoK8nkOZ1AE6RzZSNZbdOV5BzcQv83zoXSbugwY5zPmALzU9lK1L8xqiHBBzL00CeEpqHCj"
+  "####" // place secret private key here
 );
 
 router.post(
@@ -14,6 +14,8 @@ router.post(
     const total = req.body.amount;
 
     // Create a PaymentIntent with the order amount and currency
+    // International payments requires Name address description 
+    // this form is gard coded but it can be easilt created using payment intent 
     stripe.paymentIntents
       .create({
         description: "Software development services",
